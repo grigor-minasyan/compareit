aca65db3830234245fce32458b1cbb4c3aa3c14b
Grigor Minasyan <grigorminasyan1998@gmail.com>
2023-03-29 20:09:01 -0700

    added basic header and footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 py-4">
+      <div className="container mx-auto px-4">
+        <p className="text-center text-white">
+          © 2023 My Website. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
